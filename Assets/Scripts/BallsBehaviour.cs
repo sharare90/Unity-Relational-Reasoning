3e083d1c11af0d0aeb21266831969ca553779a7a
sharare90 <sharare.zehtabian@gmail.com>
2017-10-24 17:50:44 -0400

    First commit

---
+++ Assets/Scripts/BallsBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallsBehaviour : MonoBehaviour {
+
+	public float speed;
+	public string color;
+	private Rigidbody rb;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+
+	void FixedUpdate () {
+		float moveHorizontal = Random.Range(-10.0f, 10.0f);
+		float moveVertical = Random.Range(-10.0f, 10.0f);
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement * speed);
+	}
+}
