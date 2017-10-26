8d5612dc757eeb8e2a3d2555eb7bc1bffda123b8
sharare90 <sharare.zehtabian@gmail.com>
2017-10-26 14:42:08 -0400

    Add controller script

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Controller : MonoBehaviour {
+	public static string fileAddress = "/home/sharare/Relational/Assets/Data/Ball1.txt";
+	public StreamWriter writer;
+	private GameObject[] recordables;
 
-	// Use this for initialization
 	void Start () {
-		
+		writer = new StreamWriter(fileAddress, true);
+		recordables = GameObject.FindGameObjectsWithTag("Recordable");
 	}
 	
-	// Update is called once per frame
-	void Update () {
+
+	void FixedUpdate () {
+		foreach (GameObject recordable in recordables) {
+			BallsBehaviour bb = recordable.GetComponent<BallsBehaviour>();
+			Rigidbody rb = recordable.GetComponent<Rigidbody>();
+			writer.WriteLine(bb.transform.position.x + "," + bb.transform.position.y + "," + bb.transform.position.z + "," + rb.velocity.x + "," + rb.velocity.y + "," + rb.velocity.z + "," + bb.color);
+			writer.Flush();
+			
+			
+		}
+		
 		
 	}
 }
